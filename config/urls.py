cfaf0a24417ae8500c6cea709793d51a20d775c5
Michael <something@gmail.com>
2026-05-03 16:19:03 +0100

    minor formattings

--- config/urls.py
+++ config/urls.py
@@ -18,6 +18,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+from bookings.views import index
+
+urlpatterns = [path("admin/", admin.site.urls), path("bookings", index)]
